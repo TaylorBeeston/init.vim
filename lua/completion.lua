df57026dcb56bd9ba5384ab52a273398412fd332
Taylor Beeston <beeston.taylor@gmail.com>
2022-06-01 09:03:52 -0700

    :sparkles: Allow scrolling documentation in autocomplete

--- lua/completion.lua
+++ lua/completion.lua
@@ -28,8 +28,8 @@ cmp.setup(
         },
         mapping = cmp.mapping.preset.insert(
             {
-                ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-                ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
+                ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+                ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
                 ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
                 ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
                 ["<C-e>"] = cmp.mapping(
