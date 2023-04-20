40c64ce1dd327689acd5172433e74974e5b25ddb
Taylor Beeston <beeston.taylor@gmail.com>
2023-04-19 22:20:16 -0700

    :bug: Help python work

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -137,7 +137,31 @@ nvim_lsp.dockerls.setup({
 })
 
 -- Python
+local util = require("lspconfig/util")
+
+local path = util.path
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+
+    -- Find and use virtualenv in workspace directory.
+    for _, pattern in ipairs({ "*", ".*" }) do
+        local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+        if match ~= "" then
+            return path.join(path.dirname(match), "bin", "python")
+        end
+    end
+
+    -- Fallback to system Python.
+    return exepath("python3") or exepath("python") or "python"
+end
+
 nvim_lsp.pyright.setup({
+    before_init = function(_, config)
+        config.settings.python.pythonPath = get_python_path(config.root_dir)
+    end,
     capabilities = capabilities,
     on_attach = on_attach,
 })
