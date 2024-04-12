3139f432fdbb98d9994039fe7bb9655c44be3ac6
Taylor Beeston <beeston.taylor@gmail.com>
2024-04-12 14:10:05 -0700

    :zap: More lazy loading

--- lua/pluginList.lua
+++ lua/pluginList.lua
@@ -309,19 +309,176 @@ require("lazy").setup({
     },
 
     -- Autocompletion
-    { "hrsh7th/cmp-nvim-lsp",     event = { "BufReadPost", "BufNewFile" } },
-    "hrsh7th/cmp-nvim-lsp-document-symbol",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-emoji",
-    "hrsh7th/nvim-cmp",
-    { "saadparwaiz1/cmp_luasnip", event = { "BufReadPost", "BufNewFile" } },
-    "ray-x/cmp-treesitter",
     {
-        "tzachar/cmp-fuzzy-buffer",
-        dependencies = { "hrsh7th/nvim-cmp", "tzachar/fuzzy.nvim" },
+        "hrsh7th/nvim-cmp",
         event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lsp-document-symbol",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-emoji",
+            "saadparwaiz1/cmp_luasnip",
+            "ray-x/cmp-treesitter",
+            {
+                "tzachar/cmp-fuzzy-buffer",
+                dependencies = { "tzachar/fuzzy.nvim" },
+                event = { "BufReadPost", "BufNewFile" },
+            },
+            "windwp/nvim-autopairs",
+            {
+                "L3MON4D3/LuaSnip",
+                event = { "BufReadPost", "BufNewFile" },
+                config = function()
+                    require("luasnip.loaders.from_snipmate").load()
+                end,
+                build = "make install_jsregexp",
+            },
+            {
+                "danymat/neogen",
+                config = function()
+                    require("neogen").setup({ enabled = true })
+                end,
+                dependencies = "nvim-treesitter/nvim-treesitter",
+            },
+        },
+        config = function()
+            local neogen = require("neogen")
+            local luasnip = require("luasnip")
+            local cmp = require("cmp")
+            local compare = require("cmp.config.compare")
+
+            cmp.setup({
+                window = {
+                    completion = {
+                        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+                        col_offset = -3,
+                        side_padding = 0,
+                    },
+                },
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = function(entry, vim_item)
+                        local kind = require("lspkind").cmp_format({
+                            mode = "symbol_text",
+                            maxwidth = 50,
+                            menu = {
+                                nvim_lsp = "LSP",
+                                nvim_lsp_document_symbol = "LSP",
+                                buffer = "BUF",
+                                fuzzy_buffer = "FUZ",
+                                path = "PATH",
+                                -- cmp_tabnine = "AI",
+                                luasnip = "SNIP",
+                                emoji = "EMOJI",
+                                cmdline = "CMD",
+                            },
+                        })(entry, vim_item)
+
+                        local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                        kind.kind = " " .. strings[1] .. " "
+                        kind.menu = "    " .. (kind.menu or "") .. " (" .. (strings[2] or "") .. ")"
+
+                        return kind
+                    end,
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+                    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+                    ["<C-e>"] = cmp.mapping({
+                        i = cmp.mapping.abort(),
+                        c = cmp.mapping.close(),
+                    }),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = function(fallback)
+                        if luasnip.expand_or_jumpable() then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+                                ""
+                            )
+                        elseif neogen.jumpable() then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes(
+                                    "<cmd>lua require('neogen').jump_next()<CR>",
+                                    true,
+                                    true,
+                                    true
+                                ),
+                                ""
+                            )
+                        else
+                            fallback()
+                        end
+                    end,
+                    ["<S-Tab>"] = function(fallback)
+                        if luasnip.jumpable(-1) then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
+                                ""
+                            )
+                        elseif neogen.jumpable() then
+                            vim.fn.feedkeys(
+                                vim.api.nvim_replace_termcodes(
+                                    "<cmd>lua require('neogen').jump_prev()<CR>",
+                                    true,
+                                    true,
+                                    true
+                                ),
+                                ""
+                            )
+                        else
+                            fallback()
+                        end
+                    end,
+                }),
+                sorting = {
+                    priority_weight = 2,
+                    comparators = {
+                        compare.score,
+                        require("cmp_fuzzy_buffer.compare"),
+                        compare.offset,
+                        compare.exact,
+                        compare.recently_used,
+                        compare.kind,
+                        compare.sort_text,
+                        compare.length,
+                        compare.order,
+                    },
+                },
+                sources = cmp.config.sources({
+                    { name = "jupynium",     priority = 1000 },
+                    { name = "nvim_lsp",     priority = 100 },
+                    { name = "luasnip",      priority = 99 },
+                    { name = "path" },
+                    -- {name = "cmp_tabnine"},
+                    { name = "treesitter" },
+                    { name = "fuzzy_buffer", max_item_count = 5, priority = 1 },
+                    { name = "emoji" },
+                }),
+            })
+
+            cmp.setup.cmdline("/", {
+                sources = cmp.config.sources({ { name = "nvim_lsp_document_symbol" } }, { { name = "buffer" } }),
+                mapping = cmp.mapping.preset.cmdline(),
+            })
+            cmp.setup.cmdline(":", {
+                sources = cmp.config.sources({ { name = "path" }, { name = "buffer" }, { name = "cmdline" } }),
+                mapping = cmp.mapping.preset.cmdline(),
+            })
+
+            -- Autopairs
+            require("nvim-autopairs").setup()
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+        end,
     },
     { "SidOfc/mkdx",                  ft = "markdown" },
     {
@@ -352,17 +509,11 @@ require("lazy").setup({
             require("aidoc").setup({ width = 65, keymap = "<leader>d" })
         end,
     },
-    {
-        "danymat/neogen",
-        config = function()
-            require("neogen").setup({ enabled = true })
-        end,
-        dependencies = "nvim-treesitter/nvim-treesitter",
-    },
 
     -- UI
     {
         "folke/noice.nvim",
+        event = "VeryLazy",
         config = function()
             require("noice").setup({
                 lsp = {
@@ -389,7 +540,6 @@ require("lazy").setup({
         dependencies = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
-            "hrsh7th/nvim-cmp",
         },
     },
     {
@@ -554,10 +704,6 @@ require("lazy").setup({
         after = { "nvim-cmp" },  -- if a completion plugin is using tabs load it before
     },
 
-    -- Close things like { and [ and --
-    -- use 'cohama/lexima.vim'
-    "windwp/nvim-autopairs",
-
     -- Syntax checking/highlighting
     "mboughaba/i3config.vim",
 
@@ -572,8 +718,8 @@ require("lazy").setup({
     },
 
     -- Lua
-    "tjdevries/nlua.nvim",
-    "rafcamlet/nvim-luapad",
+    { "tjdevries/nlua.nvim",   ft = "lua" },
+    { "rafcamlet/nvim-luapad", ft = "lua" },
 
     -- JS
     {
@@ -592,14 +738,6 @@ require("lazy").setup({
     -- Code snippets plugin
     "epilande/vim-react-snippets",
     "epilande/vim-es2015-snippets",
-    {
-        "L3MON4D3/LuaSnip",
-        event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            require("luasnip.loaders.from_snipmate").load()
-        end,
-        build = "make install_jsregexp",
-    },
 
     -- Smooth scrolling with <C u> and <C d>
     { "psliwka/vim-smoothie",    event = { "BufReadPost", "BufNewFile" } },
